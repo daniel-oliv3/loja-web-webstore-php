8c900d3c93b5477ab50218cf143876bd01020f04
Daniel Oliveira <39778932+daniel-oliv3@users.noreply.github.com>
2022-03-31 02:19:29 -0300

    Create index.php

---
+++ index.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+	<meta charset="UTF-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<title>Loja Web</title>
+	<link rel="stylesheet" href="css/style.css">
+	<link rel="shortcut icon" href="img/php.ico"/>
+</head>
+<body>
+	
+	<script src="js/script.js"></script>
+</body>
+</html>
+
+<!--
+    Autor: Daniel Oliveira
+    Email: danieloliveira.webmaster@gmail.com
+    Manaus/Amazonas
+    31/03/2022
+-->
