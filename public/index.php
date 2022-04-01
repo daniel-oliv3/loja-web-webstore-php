5a850187ea7902646b6ea357d058d75a894cee49
Daniel Oliveira <39778932+daniel-oliv3@users.noreply.github.com>
2022-04-01 09:44:40 -0300

    Update

--- public/index.php
+++ public/index.php
@@ -5,14 +5,30 @@
 	<meta http-equiv="X-UA-Compatible" content="IE=edge">
 	<meta name="viewport" content="width=device-width, initial-scale=1.0">
 	<title>Loja Web</title>
-	<link rel="stylesheet" href="css/style.css">
-	<link rel="shortcut icon" href="img/php.ico"/>
+	<link rel="stylesheet" href="assets/css/style.css">
+	<link rel="shortcut icon" href="assets/img/php.ico"/>
 </head>
 <body>
 
 	<?php
-	
-		echo "teste";
+		//Abrir a Sessão
+		session_start();
+
+		//Carregar o Config
+		require_once('../config.php');
+
+
+
+
+
+		/*
+		Carregar o config
+		Carregar classes
+		Carregar o sistema de rotas
+			- mostrar loja
+			- mostrar carinho
+			- mostrar o backoffice, etc... 
+		*/
 
 	?>
 	
